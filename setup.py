c9595cbb208796d1e895ddd2ee4c1a8bb8d8529d
lwinhong <31954501+lwinhong@users.noreply.github.com>
2024-12-26 10:26:39 +0800

    加入打包相关

--- setup.py
+++ setup.py
@@ -5,17 +5,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py_diff_pdf2",
-    version="1.0.0",
+    version="1.0.2",
     author="lwinhong",
-    url='xxx',
-    author_email="xxx",
-    description="xxx",
+    url='https://github.com/lwinhong/py-diff-pdf',
+    author_email="",
+    description="pdf文档比对",
     long_description=long_description,
     long_description_content_type="text/markdown",
     package_dir={"": "src"},
     packages=setuptools.find_packages(
         where="src",
-        include=["py_diff_pdf2", "py_diff_pdf2.libs.*", "py_diff_pdf2.*"], ),
+        include=["py_diff_pdf2", "py_diff_pdf2.libs.*", "py_diff_pdf2.*"],
+        exclude=["py_diff_pdf2.*.pdf"]),
     python_requires=">=3.6",
     include_package_data=True
 )
